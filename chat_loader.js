0869cee61b666246370a8fb48558c776048e87ba
simplemarkcoin <actionszam@gmail.com>
2025-10-15 20:56:16 +0100

    Create chat_loader.js

---
+++ chat_loader.js
@@ -0,0 +1,12 @@
+// File Name: chat_loader.js
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+// This is your unique Tawk.to URL:
+s1.src='https://embed.tawk.to/68eff9c3cf0e41195453b0c3/1j7kmf9as';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
